f2eab130366e33e8698695ffb3f82c293caed1ed
Logan Someone <loganqqq@outlook.com>
2022-08-23 13:27:37 -0400

    Change to Guild commands

--- server/bot/deploy-commands.js
+++ server/bot/deploy-commands.js
@@ -1,15 +1,17 @@
 const { SlashCommandBuilder, Routes } = require('discord.js');
 const { REST } = require('@discordjs/rest');
 const clientId = 895694315492343832
+const guildId = 783416129908899860
 const { token } = require("../config.json");
 
 const commands = [
-	new SlashCommandBuilder().setName('Resources').setDescription('Replies with your Resources!'),
+	new SlashCommandBuilder().setName('resources').setDescription('Replies with your Resources!'),
 ]
 	.map(command => command.toJSON());
 
 const rest = new REST({ version: '10' }).setToken(token);
 
-rest.put(Routes.applicationCommands(clientId), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
-	.catch(console.error);
+await rest.put(
+	Routes.applicationGuildCommands(clientId, guildId),
+	{ body: commands },
+);
