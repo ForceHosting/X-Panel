aed1e75a879cb87f0579cb40ac559d6e0747ff93
Logan Someone <loganqqq@outlook.com>
2022-08-23 14:51:03 -0400

    And fixed some stuff

--- server/bot/deploy-commands.js
+++ server/bot/deploy-commands.js
@@ -11,7 +11,18 @@ const commands = [
 
 const rest = new REST({ version: '10' }).setToken(token);
 
-await rest.put(
-	Routes.applicationGuildCommands(clientId, guildId),
-	{ body: commands },
-);
+
+(async () => {
+	try {
+		console.log('Started refreshing application (/) commands.');
+
+		await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },
+		);
+
+		console.log('Successfully reloaded application (/) commands.');
+	} catch (error) {
+		console.error(error);
+	}
+})();
