2dba5482c4c7af06ccac4c61d168cd7027f83253
The MG | GallearTech <63264652+The-MG@users.noreply.github.com>
2022-08-24 15:43:43 -0700

    Add user resource removal logic

--- server/controllers/serverController.js
+++ server/controllers/serverController.js
@@ -8,6 +8,20 @@ module.exports.addToQueue = async (req, res, next) => {
     try {
         const { userUid, name, location, software, memory, disk, cpu } = req.body;
         const user = await User.findOne({ userUid });
+        const newTotalMem = user.availMem - memory;
+        const newTotalDisk = user.availDisk - disk;
+        const newTotalCPU = user.availCPU - cpu;
+        const newTotalSlots = user.availSlots - 1;
+        if(newTotalMem < 0){
+          return res.json({ added: false, msg: "You can't use more memory than your account has."})
+        }else if(newTotalDisk < 0){
+          return res.json({ added: false, msg: "You can't use more disk space than your account has."})
+        }else if(newTotalCPU < 0){
+          return res.json({ added: false, msg: "You can't use more CPU than your account has."})
+        }else if(newTotalSlots < 0){
+          return res.json({ added: false, msg: "You can't use more slots than your account has."})
+        }else{
+        await User.findByIdAndUpdate(user._id, {'availMem': newTotalMem, 'availDisk': newTotalDisk, 'availCPU': newTotalCPU, 'availSlots': newTotalSlots});
         const server = await Queue.create({
             serverName: name,
             serverSoftware: software,
@@ -20,6 +34,7 @@ module.exports.addToQueue = async (req, res, next) => {
           delete user.password
           addedToQueue(user.username, name, memory, cpu, disk)
         return res.json({ added: true, server });
+      }
       } catch (ex) {
         next(ex);
       }
