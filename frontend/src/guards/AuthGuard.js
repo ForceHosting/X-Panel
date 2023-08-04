61bce51936d1b18b0d357d8bb1e00dd8904f86ee
The MG | GallearTech <63264652+The-MG@users.noreply.github.com>
2023-08-04 00:09:30 -0700

    FIX WSD

--- frontend/src/guards/AuthGuard.js
+++ frontend/src/guards/AuthGuard.js
@@ -1,16 +1,10 @@
 import PropTypes from 'prop-types';
 import { useState, useEffect } from 'react';
-import { Navigate, useLocation } from 'react-router-dom';
-// hooks
+import { Navigate, useLocation, useNavigate } from 'react-router-dom';
 import jwtDecode from 'jwt-decode';
 import useAuth from '../hooks/useAuth';
-
-// components
 import LoadingScreen from '../components/LoadingScreen';
 import { PATH_AUTH, PATH_PAGE } from '../routes/paths';
-
-
-// ----------------------------------------------------------------------
 
 AuthGuard.propTypes = {
   children: PropTypes.node,
@@ -18,56 +12,43 @@ AuthGuard.propTypes = {
 
 export default function AuthGuard({ children }) {
   const { isInitialized } = useAuth();
-
   const { pathname } = useLocation();
+  const navigate = useNavigate(); // Add the useNavigate hook
 
   const [requestedLocation, setRequestedLocation] = useState(null);
   const [authenticated, setAuth] = useState();
-    
+
   useEffect(() => {
-    const token = localStorage.getItem('token')
+    if (!localStorage.getItem('token')) {
+      setAuth(false);
+      navigate(PATH_AUTH.login); // Use navigate instead of return <Navigate />
+    } else {
+      const token = localStorage.getItem('token');
       const decoded = jwtDecode(token);
-    if(!localStorage.getItem('token')){
-      setAuth(false)
-      return <Navigate to={PATH_AUTH.login} />
+      if (decoded.isBanned === true) {
+        setAuth(false);
+        navigate(PATH_PAGE.banned); // Use navigate instead of return <Navigate />
+      } else if (decoded.exp * 1000 < Date.now()) {
+        setAuth(false);
+        localStorage.removeItem('token');
+        navigate(PATH_AUTH.login); // Use navigate instead of return <Navigate />
+      } else {
+        setAuth(true);
+      }
     }
-    if (localStorage.getItem('token') === undefined){
-      setAuth(false)
-      return <Navigate to={PATH_AUTH.login} />;
-    }
-    if (localStorage.getItem('token') === "undefined"){
-      setAuth(false)
-      return <Navigate to={PATH_AUTH.login} />;
-    }
-    if(localStorage.getItem('token') === null) {
-      setAuth(false)
-      return <Navigate to={PATH_AUTH.login} />;
-    }
-    if(decoded.isBanned === true){
-      setAuth(false)
-      return <Navigate to={PATH_PAGE.banned} />;
-    }
-    if(decoded.exp * 1000 < Date.now()){
-      setAuth(false)
-      localStorage.removeItem('token')
-      return <Navigate to={PATH_AUTH.login} />;
-    }
-      setAuth(true)
-    
-  }, [])
-
-
+  }, [navigate]); // Add navigate to the dependency array to avoid missing updated navigate reference
 
   if (!isInitialized) {
     return <LoadingScreen />;
   }
-  
+
   if (authenticated === false) {
     if (pathname !== requestedLocation) {
       setRequestedLocation(pathname);
     }
     return <Navigate to={PATH_AUTH.login} />;
   }
+
   if (requestedLocation && pathname !== requestedLocation) {
     setRequestedLocation(null);
     return <Navigate to={requestedLocation} />;
